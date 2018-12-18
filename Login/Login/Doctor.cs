e02db07c5eb849e75a8318d28ca6ddcbee2bc687
Brando Curipallo <brando25_96@hotmail.com>
2018-12-18 16:45:08 -0500

    Creacion de la clase Doctor que hereda de la clase Persona

---
+++ Login/Login/Doctor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    public class Doctor : Persona
+    {
+        private string usuarioDoctor;
+        private string contraseniaDoctor;
+        private string especialidad;
+
+        public Doctor()
+        {
+        }
+
+        public Doctor(string cedula, string nombres, string apellidoPaterno, string apellidoMaterno, DateTime fechaNacimiento, int edad, string sexo, string correoElectronico, string provincia, string ciudad, string direccion, string telefono, string usuarioDoctor, string contraseniaDoctor, string especialidad) : base(cedula, nombres, apellidoPaterno, apellidoMaterno, fechaNacimiento, edad, sexo, correoElectronico, provincia, ciudad, direccion, telefono)
+        {
+            this.UsuarioDoctor = usuarioDoctor;
+            this.ContraseniaDoctor = contraseniaDoctor;
+            this.Especialidad = especialidad;
+        }
+
+        public string UsuarioDoctor { get => usuarioDoctor; set => usuarioDoctor = value; }
+        public string ContraseniaDoctor { get => contraseniaDoctor; set => contraseniaDoctor = value; }
+        public string Especialidad { get => especialidad; set => especialidad = value; }
+    }
+}
