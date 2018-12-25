31a80c7a098f4fd13827044cd43a30286eef0ec5
Brando Curipallo <brando25_96@hotmail.com>
2018-12-25 18:07:40 -0500

    Creacion de la clase Paciente q heredad de Persona

---
+++ Login/Login/Paciente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    public class Paciente : Persona
+    {
+        private string contraseniaPaciente;
+
+        public Paciente()
+        {
+        }
+
+
+        public Paciente(string cedula, string nombres, string apellidoPaterno, string apellidoMaterno, DateTime fechaNacimiento, int edad, string sexo, string correoElectronico, string provincia, string ciudad, string direccion, string telefono, string contraseniaPaciente) : base(cedula, nombres, apellidoPaterno, apellidoMaterno, fechaNacimiento, edad, sexo, correoElectronico, provincia, ciudad, direccion, telefono)
+        {
+            this.ContraseniaPaciente = contraseniaPaciente;
+        }
+
+        public string ContraseniaPaciente { get => contraseniaPaciente; set => contraseniaPaciente = value; }
+    }
+}
